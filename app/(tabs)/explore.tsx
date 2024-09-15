35ac978a949088b0aed504fe64d2666a0caf0e9d
Victor Quintana <435victorjavier@gmail.com>
2024-09-14 23:43:54 -0600

    add tabs

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>weird 2. </Text>
     </View>
   );
 }
